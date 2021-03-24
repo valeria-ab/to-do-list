551589845ee969445bba6865f48ebb8037dcf06e
valeria-ab <lera.magnum@gmail.com>
2021-03-25 00:16:49 +0300

    Updates

---
+++ precache-manifest.c744c5844c3e0f99579c2ac807008ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a62b607d6ec69f023c4898335d1bf6",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "c68ab0e2ab81b60562fe",
+    "url": "/to-do-list/static/css/main.6f830d2b.chunk.css"
+  },
+  {
+    "revision": "ea238f6057117e944196",
+    "url": "/to-do-list/static/js/2.09c7e011.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/to-do-list/static/js/2.09c7e011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68ab0e2ab81b60562fe",
+    "url": "/to-do-list/static/js/main.16acf5b0.chunk.js"
+  },
+  {
+    "revision": "6c26975b57d8d730a898",
+    "url": "/to-do-list/static/js/runtime-main.59ffd6f3.js"
+  }
+]);
